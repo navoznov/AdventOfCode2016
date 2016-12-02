a0676b0e9d06da906c339eb7c159fe0eb82a2161
imbeat <ivan.navoznov@gmail.com>
2016-12-02 08:41:02 +0300

    prepearing for part 2

--- AdventOfCode2016/Day02/Program.cs
+++ AdventOfCode2016/Day02/Program.cs
@@ -7,6 +7,18 @@ namespace Day02
     {
         static void Main(string[] args)
         {
+            var part1 = new Part1();
+            part1.Run();
+            //var part2 = new Part2();
+            //part2.Run();
+        }
+
+    }
+
+    public class Part1
+    {
+        public void Run()
+        {
             var input = File.ReadAllText("input1.txt");
             var lines = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
             var x = 0;
@@ -49,14 +61,72 @@ namespace Day02
                         }
                     }
                 }
-                Console.Write(GetNumberByCoords(x,y));
+                Console.Write(GetNumberByCoords(x, y));
             }
             Console.WriteLine();
         }
 
-        static int GetNumberByCoords(int x, int y)
+        int GetNumberByCoords(int x, int y)
         {
             return (x + 2) + (y + 1)*3;
         }
+
     }
+    //public class Part2
+    //{
+    //    public void Run()
+    //    {
+    //        var input = File.ReadAllText("input1.txt");
+    //        var lines = input.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+    //        var x = 0;
+    //        var y = 0;
+
+    //        foreach (var line in lines)
+    //        {
+    //            foreach (var ch in line)
+    //            {
+    //                if (ch == 'U')
+    //                {
+    //                    y--;
+    //                    if (y < -1)
+    //                    {
+    //                        y++;
+    //                    }
+    //                }
+    //                else if (ch == 'D')
+    //                {
+    //                    y++;
+    //                    if (y > 1)
+    //                    {
+    //                        y--;
+    //                    }
+    //                }
+    //                else if (ch == 'L')
+    //                {
+    //                    x--;
+    //                    if (x < -1)
+    //                    {
+    //                        x++;
+    //                    }
+    //                }
+    //                else if (ch == 'R')
+    //                {
+    //                    x++;
+    //                    if (x > 1)
+    //                    {
+    //                        x--;
+    //                    }
+    //                }
+    //            }
+    //            Console.Write(GetNumberByCoords(x, y));
+    //        }
+    //        Console.WriteLine();
+    //    }
+
+    //    int GetNumberByCoords(int x, int y)
+    //    {
+    //        return (x + 2) + (y + 1)*3;
+    //    }
+
+    //}
 }
