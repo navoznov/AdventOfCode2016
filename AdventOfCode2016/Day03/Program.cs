e83629c3e2b2fa966f85919c91e5cb7b41a2451b
imbeat <ivan.navoznov@gmail.com>
2016-12-03 09:35:47 +0300

    day03 part1

---
+++ AdventOfCode2016/Day03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Part1().Run();
+        }
+    }
+
+    internal class Part1
+    {
+        public void Run()
+        {
+            var lines = File.ReadAllLines("input1.txt");
+            var counter = 0;
+            foreach (var line in lines)
+            {
+                var orderedSides = line
+                    .Split(new [] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .OrderByDescending(x => x)
+                    .ToArray();
+                if (orderedSides[0]<orderedSides[1]+orderedSides[2])
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
