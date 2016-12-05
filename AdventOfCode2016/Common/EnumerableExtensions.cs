676aa51931947b849d8404fc26c8b114f64d9ca4
imbeat <ivan.navoznov@gmail.com>
2016-12-05 20:47:19 +0300

    day04 part 1 not solved yet +(

---
+++ AdventOfCode2016/Common/EnumerableExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common
+{
+    public static class EnumerableExtensions
+    {
+        public static int GetIndexOf<T>(this IEnumerable<T> source, Func<T, bool> condition)
+        {
+            var enumerator = source.GetEnumerator();
+            var index = -1;
+            while (enumerator.MoveNext())
+            {
+                index++;
+                if (condition(enumerator.Current))
+                {
+                    return index;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
