80eb251c006a7092dae5d553987618a4a0c42ad8
imbeat <ivan.navoznov@gmail.com>
2016-12-06 09:03:31 +0300

    day06 parts 1 and 2

---
+++ AdventOfCode2016/Day06/Part2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Common;
+
+namespace Day06
+{
+    public class Part2 : IPart<string>
+    {
+        public string Solve()
+        {
+            var input = GetInput();
+            var lines = input.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            int messageLength = lines[0].Length;
+            const int abcLength = 26;
+            var counterList = Enumerable.Range(0, messageLength).Select(n => Enumerable.Range('a', abcLength).ToDictionary(x => (char)x, x => 0)).ToList();
+
+            for (var i = 0; i < lines.Length; i++)
+            {
+                var line = lines[i];
+                for (var j = 0; j < messageLength; j++)
+                {
+                    var ch = line[j];
+                    counterList[j][ch]++;
+                }
+            }
+
+            var sb = new StringBuilder();
+            for (var i = 0; i < counterList.Count; i++)
+            {
+                sb.Append(counterList[i].OrderBy(x => x.Value).First().Key);
+            }
+            return sb.ToString();
+        }
+
+        string GetInput()
+        {
+            return File.ReadAllText("input1.txt");
+        }
+    }
+}
---
+++ AdventOfCode2016/Day06/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Day06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var solve1 = new Part1().Solve();
+            Console.WriteLine(solve1);
+            var solve2 = new Part2().Solve();
+            Console.WriteLine(solve2);
+        }
+    }
+}
